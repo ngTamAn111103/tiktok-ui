b3bf4d5eca4d8c5b998db381bad72bcb86000817
ngTamAn111103 <ng.t.ann2003@gmail.com>
2025-06-20 16:07:28 +0700

    test git moi

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,36 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '../../../../assets/images';
 
 const cx = classNames.bind(styles);
 function Header() {
-  return (
-    <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
-    </header>
-  );
+    return (
+      // Toàn bộ
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* Trái */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                {/* Giữa */}
+                <div className={cx('search')}>
+                    <input
+                        placeholder="Search account and video"
+                        spellCheck={false}
+                    />
+                    <button className={cx('clear')}>
+                        <i className="fa-solid fa-circle-xmark"></i>
+                    </button>
+                    <i className={cx('fa-solid fa-spinner', 'loading')}></i>
+                    <button className={cx('search-btn')}>
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div>
+                {/* Phải */}
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
